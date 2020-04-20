22c0c66bc7bacae3f3686c9bf63310a8b5d241a8
AntoineL <antoine.laroche@akinox.com>
2020-04-19 22:33:15 -0400

    added products table migration

---
+++ Data/Migrations/20200420021811_AddProductsTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace InventheReact.Data.Migrations
+{
+    public partial class AddProductsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Label = table.Column<string>(maxLength: 256, nullable: false),
+                    Quantity = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
