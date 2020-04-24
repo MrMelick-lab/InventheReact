3b8ae5ccbfb932b92e48c66b8227eeb2faf17839
AntoineL <antoine.laroche@akinox.com>
2020-04-23 22:05:54 -0400

    Added the crud api for products

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections.Generic;
+﻿#nullable enable
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using InventheReact.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using InventheReact.Data.DataTransfer;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Http;
+using System;
 
 namespace InventheReact.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class ProductsController : ControllerBase
@@ -21,9 +27,91 @@ namespace InventheReact.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Products> Get()
+        public IActionResult GetAllProducts()
         {
-            return _context.Products.ToList();
+            return Ok(_context.Products.ToList());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetByIdAsync(Guid id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdateAsync(Guid id, ProductsDTO updatedProduct)
+        {
+            if (updatedProduct is null || updatedProduct.Quantity < 0 || string.IsNullOrWhiteSpace(updatedProduct.Label))
+            {
+                return BadRequest();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            product.Label = updatedProduct.Label;
+            product.Quantity = updatedProduct.Quantity;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateAsync([FromBody]ProductsDTO products)
+        {
+            if (products is null || products.Quantity < 0 || string.IsNullOrWhiteSpace(products.Label))
+            {
+                return BadRequest();
+            }
+
+            var createdProduct = new Products
+            {
+                Id = Guid.NewGuid(),
+                Quantity = products.Quantity,
+                Label = products.Label
+            };
+
+            await _context.AddAsync(createdProduct);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetByIdAsync), new { createdProduct.Id }, createdProduct);
         }
     }
 }
---
+++ Data/DataTransfer/ProductsDTO.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventheReact.Data.DataTransfer
+{
+    public class ProductsDTO
+    {
+        public double Quantity { get; set; }
+        public string Label { get; set; } = string.Empty;
+    }
+}
